64b5203fac5a6aa2d853c2bd80e5d396650e08cd
neorusse <neorusse@gmail.com>
2020-02-20 11:11:10 +0100

    chore: Add User1 controller logic

--- src/main/java/com/ecodencode/springbootthymeleaf/Controller/User1Controller.java
+++ src/main/java/com/ecodencode/springbootthymeleaf/Controller/User1Controller.java
@@ -1,4 +1,32 @@
 package com.ecodencode.springbootthymeleaf.Controller;
 
+import com.ecodencode.springbootthymeleaf.Model.User1;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
 public class User1Controller {
+
+  @RequestMapping("add")
+  public String toAdd(User1 user) {
+
+    // return to templates/add.html page.
+    return "add";
+  }
+
+  @RequestMapping("addUser")
+  public String add(@Valid User1 user, BindingResult result) {
+    if (result.hasErrors()) {
+
+      // return to templates/add.html page.
+      return "add";
+    }
+    System.out.println("Save user =" + user);
+
+    // return to templates/success.html page.
+    return "success";
+  }
 }
